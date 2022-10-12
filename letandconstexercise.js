ea63129ba8020b4efe67a3d497d7a08882d44c94
SharonBeth <107509578+SharonBeth@users.noreply.github.com>
2022-10-12 17:00:36 -0400

    Add files via upload

---
+++ letandconstexercise.js
@@ -0,0 +1,30 @@
+//ES5 Global Constants
+
+var PI = 3.14;
+PI = 42;
+
+re-Written
+
+let PI = 3.14;
+PI = 42;
+
+const PI =3.14;
+
+if(statement === True){
+    PI = 42;
+    console.log(PI) //42
+};
+
+console.log (PI)  //3.14
+
+
+
+//    Quiz
+
+// var can be redeclared and let can NOT be.
+
+// var can be reassigned, and const can NOT be.
+
+//let can be reassigned, and const can NOT be.
+
+//hoisting is acknowledging that a variable is identifed, but not declared yet in the code.
